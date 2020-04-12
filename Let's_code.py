06b5e0fd289662813a90f2ebad214cbedb22a0c6
AvirathHegde <63294379+AvirathHegde@users.noreply.github.com>
2020-04-12 18:26:59 +0530

    Created Let's_code.py
    Snippets to 
    1. assign elements to different list.
    2.to access element from a tuple.
    3.to delete different dictionary element

---
+++ Let's_code.py
@@ -0,0 +1,18 @@
+#snippet to assign elements to different list.
+
+L1=list(input("Enter the elements : "))  
+
+#snippet to access element from a tuple.
+
+my_tuple = ('a','b','c','d','e','f')     
+print(my_tuple[4])
+#will print out the element in index 4 that is 'e'.
+
+#snippet to delete different dictionar element
+my_Dict={
+    "Fruit1":"Apple",
+    "Fruit2":"Mango",
+    "Fruit3":"Strawberry",
+}
+del my_Dict["fruit1"]
+print(my_Dict)
